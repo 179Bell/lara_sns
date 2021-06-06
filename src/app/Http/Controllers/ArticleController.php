889796bb8829046601a02544135cf84f9bf7095f
shingo-suzumura <shingo.msdf@gmail.com>
2021-06-06 22:00:43 +0900

    firstcommit

---
+++ src/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+        
+        return view('articles.index',['articles'=>$articles]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request,Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('top');
+    }
+    
+    public function edit(Article $article)
+    {
+        return view('articles.edit',['article'=>$article]);
+    }    
+
+    public  function update(ArticleRequest $request,Article $article)
+    {
+        $article->fill($request->all())->save();
+        return redirect()->route('top');
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return redirect()->route('top');
+    }
+
+    public function show(Article $article)
+    {
+        dd($article);
+        // return view('articles.show',['article'=>$article]);
+    }
+}
---
+++ src/resources/views/articles/form.blade.php
@@ -0,0 +1,19 @@
+@csrf
+<div class="md-form">
+  <label>タイトル</label>
+  <input type="text" name="title" class="form-control" value="{{ $article->title ?? old('title') }}">
+</div>
+<div class="text-danger">
+    <p>{{ $errors->first('title')}} </p>
+</div>
+<div class="form-group">
+  <label></label>
+  <textarea 
+    name="body" 
+    class="form-control" 
+    rows="16" 
+    placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
+    <div class="text-danger">
+        <p>{{ $errors->first('body') }}</p>
+    </div>
+</div>
